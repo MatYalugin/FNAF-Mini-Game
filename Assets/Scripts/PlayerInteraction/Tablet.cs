99c7decd126c381044051a4b7028fcada577be29
MAT-ZENBOOK\unity <matyaluginx@gmail.com>
2023-05-17 14:59:42 +0300

    Tablet use delay added

--- Assets/Scripts/PlayerInteraction/Tablet.cs
+++ Assets/Scripts/PlayerInteraction/Tablet.cs
@@ -7,6 +7,7 @@ public class Tablet : MonoBehaviour
     public GameObject resourcesManager;
     public GameObject tablet;
     public bool isWorking = true;
+    private bool isReady = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +17,21 @@ public class Tablet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && tablet.activeSelf == false && isWorking != false)
+        if (Input.GetKeyDown(KeyCode.Space) && tablet.activeSelf == false && isWorking != false && isReady != false)
         {
             tablet.SetActive(true);
             resourcesManager.GetComponent<Energy>().tabletOn = true;
+            isReady = false;
+            Invoke("makeReadyToUse", 1.2f);
         }
         else if (Input.GetKeyDown(KeyCode.Space) && tablet.activeSelf == true)
         {
             resourcesManager.GetComponent<Energy>().tabletOn = false;
             tablet.SetActive(false);
         }
+    }
+    public void makeReadyToUse()
+    {
+        isReady = true;
     }
 }
