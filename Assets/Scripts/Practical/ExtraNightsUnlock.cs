d94f17dc52f833ad8909a62b4f4dd2c4ae7c1195
MAT-ZENBOOK\unity <matyaluginx@gmail.com>
2023-05-20 16:37:25 +0300

    Weighty bugs with previous update fixed

--- Assets/Scripts/Practical/ExtraNightsUnlock.cs
+++ Assets/Scripts/Practical/ExtraNightsUnlock.cs
@@ -8,6 +8,7 @@ public class ExtraNightsUnlock : MonoBehaviour
     public bool saving = true;
     public int nightToSave = 1;
     private int nightSaved;
+    private int hardNightPassed;
     public Button hardNightButton;
     public Button unrealNightButton;
     // Start is called before the first frame update
@@ -27,15 +28,31 @@ public class ExtraNightsUnlock : MonoBehaviour
     public void saveNight()
     {
         PlayerPrefs.SetInt("lastNight", nightToSave);
+        if (nightToSave == 2)
+        {
+            PlayerPrefs.SetInt("lastNight", nightToSave);
+            PlayerPrefs.SetInt("hardNightPassed", 1);
+        }
     }
     public void unlockNightInMainMenu()
     {
         nightSaved = PlayerPrefs.GetInt("lastNight");
-        if(nightSaved == 1)
+        hardNightPassed = PlayerPrefs.GetInt("hardNightPassed");
+        if (nightSaved == 1)
         {
             hardNightButton.interactable = true;
         }
         if (nightSaved == 2)
+        {
+            hardNightButton.interactable = true;
+            unrealNightButton.interactable = true;
+        }
+        if (nightSaved == 1 && hardNightPassed == 1)
+        {
+            hardNightButton.interactable = true;
+            unrealNightButton.interactable = true;
+        }
+        if (nightSaved == 3)
         {
             hardNightButton.interactable = true;
             unrealNightButton.interactable = true;
